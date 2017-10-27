473d3ce1290281396f2a25afb7678b76f54d07b8
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2017-10-27 15:03:47 +0800

    Initialize Files

---
+++ src/components/Answer.vue
@@ -0,0 +1,23 @@
+<template>
+  <transition enter-active-class='animated flipOutY'
+          leave-active-class='animated flipOutY' mode='out-in'>
+    <div class="alert alert-success text-center">
+        <h1>That's Correct!</h1>
+        <hr>
+        <button class="btn btn-primary" @click="onNextQuestion">Next Question</button>
+      </div>
+    </transition>
+</template>
+<style>
+
+</style>
+<script>
+
+    export default{
+        methods: {
+            onNextQuestion() {
+                this.$emit('confirmed');
+            }
+        }
+    }
+</script>
